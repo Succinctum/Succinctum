b44fc7edda0c0a543e51200f63010bccb5e8e7e9
Ibrahiem Rasul <88404728+IboRasul@users.noreply.github.com>
2023-12-08 00:25:37 +0300

    web page except nodemodules

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
+// module.exports = {
+
+//   plugins: [
+//       require('flowbite/plugin')
+//   ]
+
+// }
